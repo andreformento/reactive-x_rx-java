858807e554683abe2d697f8df9a201747754eaac
André Formento <andreformento.sc@gmail.com>
2018-05-29 15:30:57 +0000

    Add spring reactive example

---
+++ spring-reactivex-examples/src/main/java/com/formento/reactivex/reactivexexamples/ReactivexExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.formento.reactivex.reactivexexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivexExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivexExamplesApplication.class, args);
+	}
+}
