79040f604218ca8549c4cac7ae25d968335534a4
André Formento <andreformento.sc@gmail.com>
2018-05-29 15:51:05 +0000

    Add controller

---
+++ spring-reactivex-examples/README.md
@@ -0,0 +1,4 @@
+# Spring reactivex examples
+
+- https://docs.spring.io/spring/docs/current/spring-framework-reference/web-reactive.html#spring-webflux
+- https://docs.spring.io/spring/docs/current/spring-framework-reference/web-reactive.html#webflux-ann-modelattrib-method-args
---
+++ spring-reactivex-examples/src/main/java/com/formento/reactivex/reactivexexamples/PersonController.java
@@ -0,0 +1,27 @@
+package com.formento.reactivex.reactivexexamples;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/persons")
+class PersonController {
+
+    @GetMapping("/{id}")
+    public PersonDTO getPerson(@PathVariable String id) {
+        return new PersonDTO(id, "Some Name", 18);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void add(@RequestBody PersonDTO person) {
+        // ...
+    }
+
+}
