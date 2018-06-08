980fc9d60f53942669b4db779645c7e7b49350d5
André Formento <andreformento.sc@gmail.com>
2018-06-08 16:13:39 +0000

    Examples

--- spring-reactivex-examples/src/test/java/com/formento/reactivex/reactivexexamples/MergeWith.java
+++ spring-reactivex-examples/src/test/java/com/formento/reactivex/reactivexexamples/MergeWith.java
@@ -4,7 +4,7 @@ import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
-public class ConcatWith2 {
+public class MergeWith {
 
     static class Request {
         private final String query;
